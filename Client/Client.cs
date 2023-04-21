48974ec494e7d7663861504bf9a71524c787ff41
h3lm3tleet <dajes007@gmail.com>
2023-04-21 21:45:20 +0300

    Added Pet Clinic project

---
+++ Client/Client.cs
@@ -0,0 +1,105 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ServiceModel;
+using System.Threading;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(500);
+            var serviceAddress = "127.0.0.1:10000";
+            var serviceName = "MyService";
+
+            Uri tcpUri = new Uri($"net.tcp://{serviceAddress}/{serviceName}");
+            EndpointAddress address = new EndpointAddress(tcpUri);
+            NetTcpBinding clientBinding = new NetTcpBinding();
+            ChannelFactory<ITransferObject> factory = new ChannelFactory<ITransferObject>(clientBinding, address);
+            var service = factory.CreateChannel();
+
+            while (true)
+            {
+                {
+                    Console.WriteLine("Выберите действие:");
+                    Console.WriteLine("1. Вывести список всех таблиц");
+                    Console.WriteLine("2. Вывести всю таблицу");
+                    Console.WriteLine("3. Добавить клиента - требуется перед добавлением питомца");
+                    Console.WriteLine("4. Добавить питомца");
+                    Console.WriteLine(new String('-', 50));
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            List<string> tableNames = service.ShowAllTables();
+                            foreach (string tableName in tableNames)
+                            {
+                                Console.WriteLine(tableName);
+                            }
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Введите название таблицы: ");
+                            string tableChoosenName = Console.ReadLine();
+                            DataTable dataTable = service.GetTable(tableChoosenName);
+                            int[] columnWidths = new int[dataTable.Columns.Count];
+                            for (int i = 0; i < dataTable.Columns.Count; i++)
+                            {
+                                columnWidths[i] = dataTable.Columns[i].ColumnName.Length;
+                                foreach (DataRow row in dataTable.Rows)
+                                {
+                                    if (row[i].ToString().Length > columnWidths[i])
+                                    {
+                                        columnWidths[i] = row[i].ToString().Length;
+                                    }
+                                }
+                            }
+
+                            for (int i = 0; i < dataTable.Columns.Count; i++)
+                            {
+                                Console.Write(dataTable.Columns[i].ColumnName.PadRight(columnWidths[i] + 2));
+                            }
+                            Console.WriteLine();
+
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                for (int i = 0; i < dataTable.Columns.Count; i++)
+                                {
+                                    Console.Write(row[i].ToString().PadRight(columnWidths[i] + 2));
+                                }
+                                Console.WriteLine();
+                            }
+                            break;
+                        case 3:
+                            {
+                                Console.WriteLine(" Введите значения для таблицы Owners, разделенные запятыми: \n " +
+                                                 "OwnerID, FirstName, LastName, PhoneNumber");
+                                string owners = Console.ReadLine();
+                                string[] ownersTable = owners.Split(',');
+                                service.AddOwner(ownersTable);
+                                break;
+                            }
+
+                        case 4:
+                            {
+                                Console.WriteLine(" Введите значения для таблицы Animals, разделенные запятыми: \n " +
+                                                 "AnimalID, AnimalName, Species, Breed, OwnerID");
+                                string pets = Console.ReadLine();
+                                string[] petsTable = pets.Split(',');
+                                service.AddPet(petsTable);
+                                break;
+                            }
+                        default:
+                            Console.WriteLine("Неверное число, попробуйте снова");
+                            break;
+                    }
+                }
+                Console.WriteLine(new String('-', 50));
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace Server
+{
+    [ServiceContract]
+    public interface ITransferObject
+    {
+        [OperationContract]
+        List<string> ShowAllTables();
+        [OperationContract]
+        DataTable GetTable(string tableChoosenName);
+        [OperationContract]
+        void AddOwner(string[] ownerTable);
+        [OperationContract]
+        void AddPet(string[] petsTable);
+    }
+
+    [DataContract]
+    public class TransferObject : ITransferObject
+    {
+        public SqlConnection Connection { get; set; }
+        public TransferObject()
+        {
+            string connectionString = "Data Source=H3LM3T\\SQLEXPRESS;Initial Catalog=PetClinicVitakor;Integrated Security=True";
+            Connection = new SqlConnection(connectionString);
+            Connection.Open();
+        }
+
+
+        public List<string> ShowAllTables()
+        {
+            List<string> tableNames = new List<string>();
+            DataTable databaseSchema = Connection.GetSchema("Tables");
+            foreach (DataRow row in databaseSchema.Rows)
+            {
+                tableNames.Add(row["TABLE_NAME"].ToString());
+            }
+            return tableNames;
+        }
+
+        [OperationBehavior]
+        public DataTable GetTable(string tableChoosenName)
+        {
+            string query = $"SELECT * FROM {tableChoosenName}";
+            DataTable dataTable = new DataTable("choosenTable");
+            using (SqlCommand getTable = new SqlCommand(query, Connection))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(getTable);
+                adapter.Fill(dataTable);
+            }
+            return dataTable;
+        }
+
+        public void AddOwner(string[] ownersTable)
+        {
+            string insertData = "INSERT INTO Owners VALUES (@value1, @value2, @value3, @value4)";
+            SqlCommand insertCommand = new SqlCommand(insertData, Connection);
+
+            insertCommand.Parameters.AddWithValue("@value1", ownersTable[0]);
+            insertCommand.Parameters.AddWithValue("@value2", ownersTable[1]);
+            insertCommand.Parameters.AddWithValue("@value3", ownersTable[2]);
+            insertCommand.Parameters.AddWithValue("@value4", ownersTable[3]);
+
+            insertCommand.ExecuteNonQuery();
+        }
+
+        public void AddPet(string[] petsTable)
+        {
+            string insertData = "INSERT INTO Animals VALUES (@value1, @value2, @value3, @value4, @value5)";
+            SqlCommand insertCommand = new SqlCommand(insertData, Connection);
+
+            insertCommand.Parameters.AddWithValue("@value1", petsTable[0]);
+            insertCommand.Parameters.AddWithValue("@value2", petsTable[1]);
+            insertCommand.Parameters.AddWithValue("@value3", petsTable[2]);
+            insertCommand.Parameters.AddWithValue("@value4", petsTable[3]);
+            insertCommand.Parameters.AddWithValue("@value5", petsTable[4]);
+
+            insertCommand.ExecuteNonQuery();
+        }
+    }
+
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server is running...");
+            var serviceAddress = "127.0.0.1:10000";
+            var serviceName = "MyService";
+
+            var host = new ServiceHost(typeof(TransferObject), new Uri($"net.tcp://{serviceAddress}/{serviceName}"));
+            var serverBinding = new NetTcpBinding();
+            host.AddServiceEndpoint(typeof(ITransferObject), serverBinding, "");
+            host.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
